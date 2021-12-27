2691faa4a08a4bd9d3846d2e315f1711c8b999af
Bryan Bandela <bryan.bandela@gmail.com>
2021-12-27 23:54:30 +0200

    Final Deployment prep

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const budgetRoutes = require('./Routes/budgetRoutes');
 const tipRoutes = require('./Routes/tipRoutes');
 const { notFound, errorHandle } = require('./middleware/errorMiddleware');
 const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -22,6 +23,18 @@ app.use('/api/tips', tipRoutes);
 
 app.use(notFound);
 app.use(errorHandle);
+
+if (process.env.MODE === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running ...');
+  });
+}
 
 const port = process.env.PORT;
 
