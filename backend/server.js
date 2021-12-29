913efd81bdc2a4bcacea927ae78505eb2623dc46
Bryan Bandela <bryan.bandela@gmail.com>
2021-12-29 14:57:51 +0200

    Add middleware in server for CORS

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,27 @@ require('dotenv').config();
 connect();
 
 app.use(express.json());
-app.use(cors());
+
+// ** MIDDLEWARE ** //
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:8080',
+  'https://troooc.heroku.com',
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log('** Origin of request ' + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log('Origin acceptable');
+      callback(null, true);
+    } else {
+      console.log('Origin rejected');
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api/users', userRoutes);
 app.use('/api/transactions', transactionRoutes);
